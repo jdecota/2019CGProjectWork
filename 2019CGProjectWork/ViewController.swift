299805211b8b47db289f8fa0c88bc8b24213b7d2
Jennifer DeCota <jdecota@sbcglobal.net>
2019-01-06 07:37:09 -0600

    Initial Commit

---
+++ 2019CGProjectWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2019CGProjectWork
+//
+//  Created by Jennifer DeCota on 1/6/19.
+//  Copyright © 2019 iOS Class. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 2019CGProjectWorkTests/_019CGProjectWorkTests.swift
@@ -0,0 +1,34 @@
+//
+//  _019CGProjectWorkTests.swift
+//  2019CGProjectWorkTests
+//
+//  Created by Jennifer DeCota on 1/6/19.
+//  Copyright © 2019 iOS Class. All rights reserved.
+//
+
+import XCTest
+@testable import _019CGProjectWork
+
+class _019CGProjectWorkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
